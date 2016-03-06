225e5ac304365ce24ea22cad86a9651a07d64849
shepitko <ya.shepitko@gmail.com>
2016-03-06 20:39:17 +0200

    add to cors 'access-control-expose-headers'

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,10 @@ module TodoRailsAngular2
      config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        resource '*', 
+        :headers => :any, 
+        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
